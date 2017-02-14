23a8f1b84300cb4fe591b116a1286465c1d9326c
Jerry Jia <jerry.jia@sap.com>
2017-02-14 21:36:50 +0800

    remove ""

--- kafka_influxdb/encoder/heapster_event_json_encoder.py
+++ kafka_influxdb/encoder/heapster_event_json_encoder.py
@@ -102,9 +102,9 @@ class Encoder(object):
         
         tags = entry["EventTags"]
         if tags.get("hostname"):
-            tag.append('hostname="{0!s}"'.format(self.escape_tag(tags["hostname"])))
+            tag.append('hostname={0!s}'.format(self.escape_tag(tags["hostname"])))
         if tags.get("cluster"):
-            tag.append('cluster="{0!s}"'.format(self.escape_tag(tags["cluster"])))
+            tag.append('cluster={0!s}'.format(self.escape_tag(tags["cluster"])))
 
         tag_str = ','.join(tag) 
         msg = ev["message"].replace('\\"', '"').replace("\n", '').replace('"', '\\"')
